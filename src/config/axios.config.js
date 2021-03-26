018011f4ad1eb8407ed46c9a70406f94140f2cd5
Alfred <yang759126596@163.com>
2021-03-26 17:27:23 +0800

    Update axios.config.js

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 let BASE_URL
 let IMAGE_PREFIX
 if (isProd) {
-  BASE_URL = '' // 生产地址
-  IMAGE_PREFIX = '' // 生产地址
+  BASE_URL = 'http://106.54.82.67:8081/api/admin' // 生产地址
+  IMAGE_PREFIX = 'http://106.54.82.67' // 生产地址
 } else {
   BASE_URL = 'http://localhost:8080/api/admin'
   IMAGE_PREFIX = 'http://localhost:8080'
